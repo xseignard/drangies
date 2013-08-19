d013b9e42612997a3b9035521714ebc723921ef2
xseignard <xavier.seignard@gmail.com>
2013-08-19 12:28:20 +0200

    full grunt build

--- tools/server.js
+++ tools/server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
 	app = express();
 
-app.use(express.static(__dirname + '/../'));
+app.use(express.static(__dirname + '/../dist/'));
 app.use(express.bodyParser());
 
 var port = process.env.PORT || 8000;
