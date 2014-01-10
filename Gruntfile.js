e23b0f957bf2e3796d38008437af6e8eb656f89d
xseignard <xavier.seignard@gmail.com>
2014-01-10 15:52:42 +0100

    Updated leaplamp

--- Gruntfile.js
+++ Gruntfile.js
@@ -101,7 +101,7 @@ module.exports = function(grunt) {
           {
             expand: true,
             cwd: 'bower_components/fancybox/source',
-            src: ['jquery.fancybox.css', 'fancybox_overlay.png', 'fancybox_sprite.png', 'fancybox_loading.gif', 'blank.gif'],
+            src: ['jquery.fancybox.css', 'fancybox_overlay.png', 'fancybox_sprite.png', 'fancybox_loading.gif', 'blank.gif', 'helpers/jquery.fancybox-media.js'],
             dest: '<%= distDir.vendor %>'            
           }
         ]
